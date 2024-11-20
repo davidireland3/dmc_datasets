27c3862b17b56a41e0a18e541d96db9442c918e9
davidireland3 <david_ireland@live.co.uk>
2024-11-20 21:02:25 +0000

    - updating config

--- dmc_datasets/config.py
+++ dmc_datasets/config.py
@@ -2,4 +2,4 @@ import os
 
 def get_data_dir():
     """Get the data directory from environment variable or default."""
-    return os.environ.get('DMC_DISCRETE_DATA_DIR', os.path.expanduser('~/dmc_datasets/d4rl_format'))
+    return os.environ.get('DMC_DISCRETE_DATA_DIR')
